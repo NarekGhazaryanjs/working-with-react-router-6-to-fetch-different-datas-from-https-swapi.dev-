f8fad311a26f94cf65456b19350bb658cb566ac8
Narek Ghazaryan <narek.ghazaryan@dataart.com>
2022-08-16 19:41:16 +0400

    add new fetches

--- swapi/src/ui/pages/Films.js
+++ swapi/src/ui/pages/Films.js
@@ -3,13 +3,34 @@ import List from "../../components/List/List"
 import Listitem from "../../components/Listitem/Listitem"
 import Main from "../../components/main/Main"
 
+
+
+
 const Films = (props) => {
   const [films,setFilms] = useState([])
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/films/')
+    .then(data => data.json())
+    .then(res => res.results)
+    .then(result => setFilms(result))
+  },[])
 
   return (
     <Main>
         <List>
-           
+            {films.map((film,index) => {
+                return (
+                    <Listitem key={index}>
+                      title  {film.title} <br />
+                      episode  {film.episode_id} <br />
+                      opening crawl  {film.opening_crawl} <br />
+                      director  {film.director} <br />
+                      producer  {film.producer} <br />
+                      release date  {film.release_date} <br />
+                    </Listitem>
+                )
+            })}
         </List>
     </Main>
   )
--- swapi/src/ui/pages/Species.js
+++ swapi/src/ui/pages/Species.js
@@ -1,9 +1,49 @@
-import Main from "../../components/main/Main"
+import { useEffect, useState } from "react";
+import List from "../../components/List/List";
+import Listitem from "../../components/Listitem/Listitem";
+import Main from "../../components/main/Main";
+
+// "name": "Human", 
+// "classification": "mammal", 
+// "designation": "sentient", 
+// "average_height": "180", 
+// "skin_colors": "caucasian, black, asian, hispanic", 
+// "hair_colors": "blonde, brown, black, red", 
+// "eye_colors": "brown, blue, green, hazel, grey, amber", 
+// "average_lifespan": "120", 
+// "homeworld": "https://swapi.dev/api/planets/9/", 
+// "language": "Galactic Basic", 
 
 const Species = (props) => {
-  return (
+    const [species, setSpecies] = useState([]);
+
+    useEffect(() => {
+      fetch('https://swapi.dev/api/species/')
+      .then(data => data.json())
+      .then(result => result.results)
+      .then(res => setSpecies(res))
+    })
+  
+    return (
     <Main>
-        its Species
+        <List>
+            {species.map((specie,index) => {
+                return (
+                    <Listitem key={index}>
+                      name  {specie.name} <br />
+                      classification  {specie.classification} <br />
+                      designation  {specie.designation} <br />
+                      average height  {specie.average_height} <br />
+                      skin colors  {specie.skin_colors} <br />
+                      hair colors  {specie.hair_colors} <br />
+                      eye colors  {specie.eye_colors} <br />
+                      average lifespan  {specie.average_lifespan} <br />
+                      homeworld  {specie.homeworld} <br />
+                      language   {specie.language} <br />
+                    </Listitem>
+                )
+            })}
+        </List>
     </Main>
   )
 }
